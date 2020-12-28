e1f8138e5541ad4ed987e32bb78da92c0a9c81e0
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2020-12-28 20:38:15 +0600

    Create Java2Comment.java

---
+++ Java2Comment.java
@@ -0,0 +1,10 @@
+public class Java2Comment {
+  public static void main(String[] args) {
+  
+  	// for single line text
+  
+    /* Comments for multiline
+    large text */
+    System.out.println("Hello World");
+  }
+}
