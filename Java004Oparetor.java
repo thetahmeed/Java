990f0f348a9901d2ca063e8373a5fa348bdd91c7
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-05 18:47:14 +0600

    Added More Pro Max Ultra 5G

--- Java004Oparetor.java
+++ Java004Oparetor.java
@@ -41,3 +41,11 @@ public class Main {
 // || 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
 // !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
 
+// More Pro Max Ultra 5G: Java Bitwise Operators
+// &	AND - Sets each bit to 1 if both bits are 1	5 & 1	0101 & 0001	0001	 1
+// |	OR - Sets each bit to 1 if any of the two bits is 1	5 | 1	0101 | 0001	0101	 5
+// ~	NOT - Inverts all the bits	~ 5	 ~0101	1010	 10
+// ^	XOR - Sets each bit to 1 if only one of the two bits is 1	5 ^ 1	0101 ^ 0001	0100	 4
+// <<	Zero-fill left shift - Shift left by pushing zeroes in from the right and letting the leftmost bits fall off	9 << 1	1001 << 1	0010	2
+// >>	Signed right shift - Shift right by pushing copies of the leftmost bit in from the left and letting the rightmost bits fall off	9 >> 1	1001 >> 1	1100	12
+// >>>	Zero-fill right shift - Shift right by pushing zeroes in from the left and letting the rightmost bits fall off	
