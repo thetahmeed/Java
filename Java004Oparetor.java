ee3ecebaa3d65dde3526f08bd69530367bf09045
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-05 18:45:25 +0600

    Added More Pro Max Ultra

--- Java004Oparetor.java
+++ Java004Oparetor.java
@@ -28,10 +28,16 @@ public class Main {
 // >>=	x >>= 3	x = x >> 3	
 // <<=	x <<= 3	x = x << 3
 
-// More Pro Max:
+// More Pro Max: Java Comparison Operators
 // ==	Equal to	x == y	
 // !=	Not equal	x != y	
 // >	Greater than	x > y	
 // <	Less than	x < y	
 // >=	Greater than or equal to	x >= y	
 // <=	Less than or equal to	x <= y
+
+// More Pro Max Ultra: Java Logical Operators
+// && 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10	
+// || 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
+// !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
