b96e64743e8480e55c383d0b652779354c266d63
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-05 18:42:55 +0600

    More Pro Max

--- Java004Oparetor.java
+++ Java004Oparetor.java
@@ -27,3 +27,11 @@ public class Main {
 // ^=	x ^= 3	x = x ^ 3	
 // >>=	x >>= 3	x = x >> 3	
 // <<=	x <<= 3	x = x << 3
+
+// More Pro Max:
+// ==	Equal to	x == y	
+// !=	Not equal	x != y	
+// >	Greater than	x > y	
+// <	Less than	x < y	
+// >=	Greater than or equal to	x >= y	
+// <=	Less than or equal to	x <= y
