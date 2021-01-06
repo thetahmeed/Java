26159c569d2a520fb5e06bfb261796e4d64f041e
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-01-06 21:12:00 +0600

    Create Java006Math.java

---
+++ Java006Math.java
@@ -0,0 +1,20 @@
+public class Java006Math {
+  public static void main(String[] args) {
+  
+    // find max number
+    Math.max(5, 10));
+    
+    // find min number
+    Math.min(5, 10)
+    
+    // get square root
+    Math.sqrt(64);
+    
+    // get positive numver
+    Math.abs(-4.7);
+    
+    // get a random number (0 to 100)
+    int randomNum = (int)(Math.random() * 101);
+   
+  }
+}
