2f79c31de25dec1a388abb26f43af4412f6b03da
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-30 12:42:10 +0600

    Create Bitwise.java

---
+++ 3Operator/Bitwise.java
@@ -0,0 +1,10 @@
+public class Bitwise {
+    int a = 10;
+    int b = 20;
+    public void bitWiseAnd() {
+        System.out.println("a&b "+(a&b));
+    }
+    public void bitWiseOr() {
+        System.out.println("a|b "+(a|b));
+    }
+}
