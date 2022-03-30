18289275467bc55b05865b662a2f8dfbeefa81fc
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-30 12:42:05 +0600

    Create Arithmetic.java

---
+++ 3Operator/Arithmetic.java
@@ -0,0 +1,25 @@
+public class Arithmetic {
+    int a = 25;
+    int b = 5;
+    int c;
+    public void add() {
+        c = a+b;
+        System.out.println("a+b="+c);
+    }
+    public void sub() {
+        c = a-b;
+        System.out.println("a-b="+c);
+    }
+    public void mul() {
+        c = a*b;
+        System.out.println("a*b="+c);
+    }
+    public void div() {
+        c = a/b;
+        System.out.println("a/b="+c);
+    }
+    public void mod() {
+        c = a%b;
+        System.out.println("a+b="+c);
+    }
+}
