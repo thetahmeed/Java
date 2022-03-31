712c5f33a2bacf815d8f74665f7aa6c416cce345
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-31 12:42:17 +0600

    Create InDe.java

---
+++ 3Operator/InDe.java
@@ -0,0 +1,11 @@
+public class InDe {
+    int a=1;
+    public void Increment() {
+        a++;
+        System.out.println("a="+a);
+    }
+    public void Decrement() {
+        a--;
+        System.out.println("a="+a);
+    }
+}
