60434a4330c2c393806619a105c78622cda3b27a
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-03-31 12:42:20 +0600

    Create Logical.java

---
+++ 3Operator/Logical.java
@@ -0,0 +1,11 @@
+public class Logical {
+    int a = 20;
+    int b = 20;
+    public void bothAreEvenOrNot() {
+        if (a%2==0 && b%2==0) {
+            System.out.println("Even");
+        }else{
+            System.out.println("Not even");
+        }
+    }
+}
