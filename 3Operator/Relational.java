3f88ab6680243a9b0fe9280d1032389ed79d7c75
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-04-01 12:42:30 +0600

    Create Relational.java

---
+++ 3Operator/Relational.java
@@ -0,0 +1,12 @@
+public class Relational {
+    int a = 5;
+    int b = 7;
+    public void whoIsBigger() {
+        if(a>b){
+            System.out.println("a is bigger");
+        }
+        if(a<b){
+            System.out.println("b is bigger");
+        }
+    }
+}
