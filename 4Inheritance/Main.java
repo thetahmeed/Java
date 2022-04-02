9b15598f20cbf9ec5089a64400417cfe0d111376
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-04-02 12:42:42 +0600

    Create Main.java

---
+++ 4Inheritance/Main.java
@@ -0,0 +1,8 @@
+class Main extends AnotherClass {
+    public static void main(String[] args) {
+        System.out.println("i = "+i);
+        System.out.println("s = "+s);
+
+        printSomething();
+    }
+}
