5a5d61a257733d70e61c547ee8c9822f162c091d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-04-03 12:42:49 +0600

    Create AnotherOverrideMethods.java

---
+++ 5Polymorphism/AnotherOverrideMethods.java
@@ -0,0 +1,9 @@
+public class AnotherOverrideMethods extends OverrideMethods{
+     
+
+    // This is called method overriding
+    @Override
+    void printHello(){
+        System.out.println("Hi");
+    }
+}
