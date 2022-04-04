f3933b255e13c0d1272d3b393f98b171318969c6
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-04-04 12:43:15 +0600

    Create Main.java

---
+++ 5Polymorphism/Main.java
@@ -0,0 +1,9 @@
+public class Main extends AnotherOverrideMethods{
+    public static void main(String[] args) {
+        OverloadMethods test = new OverloadMethods();
+
+        test.iAmAMethod("Tahmeedul Islam");
+        test.iAmAMethod(5.11);
+        test.iAmAMethod(90);
+    }
+}
