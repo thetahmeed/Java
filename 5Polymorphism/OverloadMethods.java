ae5b50693e0b3204fe71c025c4c942d2d2825111
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-04-04 12:43:19 +0600

    Create OverloadMethods.java

---
+++ 5Polymorphism/OverloadMethods.java
@@ -0,0 +1,16 @@
+public class OverloadMethods {
+    
+        
+    // This is called method overloading
+    static void iAmAMethod(String name){
+        System.out.println("Your name is "+name);
+    }
+
+    static void iAmAMethod(int age){
+        System.out.println("Your age is "+age);
+    }
+
+    static void iAmAMethod(double height){
+        System.out.println("Your height is "+height);
+    }
+}
