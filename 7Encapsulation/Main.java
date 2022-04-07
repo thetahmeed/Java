f136b99916331987b173fa68dc91b51f2ff05966
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-04-07 12:43:53 +0600

    Create Main.java

---
+++ 7Encapsulation/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Encapsulation means capsulating any elements
+        // With the help with ACCESS MODIFIRES
+        // 1. Private - Only parent class.
+        // 2. Default - Only parent package.
+        // 3. Protected - Only parent package (To get on other package need to extends this class)
+        // 4. Public - Open to all
+    }
+}
