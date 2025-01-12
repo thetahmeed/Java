154707df53d53a6605a5c4ea19c655079f0c83be
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-01-12 18:17:17 +0600

    Create AppletPractice.java

---
+++ 12Applet/AppletPractice.java
@@ -0,0 +1,8 @@
+import java.applet.*;
+import java.awt.*;
+
+public class AppletPractice extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello World", 25, 90);
+    }
+}
